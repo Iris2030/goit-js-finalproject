b9e3c79ce92365ecb07ee8568f9a98f78631ec1d
Iris2030 <ibazhinova@gmail.com>
2023-10-19 20:04:23 +0300

    fix

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -57,16 +57,3 @@ function onPressEsc(event) {
 	}
 }
 
-//  const instance = basicLightbox.create(`
-//     <div class="modal">
-//         <p>A custom modal that has been styled independently. It's not part of basicLightbox, but perfectly shows its flexibility.</p>
-//         <input placeholder="Type something">
-//         <a>Close</a>
-//     </div>
-// `, {
-//     onShow: (instance) => {
-//         instance.element().querySelector('a').onclick = instance.close
-//     }
-// })
-
-// instance.show()
