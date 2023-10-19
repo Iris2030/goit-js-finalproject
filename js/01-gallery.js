c1e0f8a535616d64690886fa32486893dd4d8547
Iris_B <ibazhinova@gmail.com>
2023-10-19 20:09:53 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -3,20 +3,18 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 function galleryMaker(items) {
-	return items.map(({ preview, original, description }) => {
-		return `<div class="gallery__item">
-		<a class="gallery__link" href="${original}">
-		<img
-		    class="gallery__image"
-		    src="${preview}"
-		    data-source="${original}"
-		    alt="${description}"
-		/>
-		</a>
-		</div>`
-		
-	}).join("") 
-	
+    return items.map(({ preview, original, description }) => {
+	return `<div class="gallery__item">
+	<a class="gallery__link" href="${original}">
+	    <img
+	    class="gallery__image"
+	    src="${preview}"
+	    data-source="${original}"
+	    alt="${description}"
+	/>
+	</a>
+	</div>`	
+    }).join("") 	
 }
 
 const galleryEl = galleryMaker(galleryItems)
@@ -28,32 +26,31 @@ gallery.insertAdjacentHTML('afterbegin', galleryEl)
 gallery.addEventListener("click", onImgClick)
 
 function onImgClick(event) {
-	event.preventDefault()
+    event.preventDefault()
 	
-	createLightbox(event.target.dataset.source)
+    createLightbox(event.target.dataset.source)
 	
-	window.addEventListener("keydown", onPressEsc)    	
+    window.addEventListener("keydown", onPressEsc)    	
 }
 
 function createLightbox(image) {
-	const instance = basicLightbox.create(`
-	<img src = "${image}" width="800" height="600">
-	`,)    
+    const instance = basicLightbox.create(`
+    <img src = "${image}" width="800" height="600">
+    `,)    
 
-	instance.show()
+    instance.show()
 }
 
 function onPressEsc(event) {
-	console.log(event);
 	
-	const lightbox = document.querySelector(".basicLightbox")
-	if(event.code === 'Escape' && lightbox){
-		lightbox.classList.remove("basicLightbox--visible")
-		onModalClosed()
-	}
+    const lightbox = document.querySelector(".basicLightbox")
+    if(event.code === 'Escape' && lightbox){
+	lightbox.classList.remove("basicLightbox--visible")
+	onModalClosed()
+    }
 	
-	function onModalClosed() {
-		window.removeEventListener("keydown", onPressEsc)	
-	}
+    function onModalClosed() {
+	window.removeEventListener("keydown", onPressEsc)	
+    }
 }
 
