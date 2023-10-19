38451d7a8c56751d3fb70f79ab3e58e81217c929
Iris_B <ibazhinova@gmail.com>
2023-10-19 20:07:07 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -7,10 +7,10 @@ function galleryMaker(items) {
 		return `<div class="gallery__item">
 		<a class="gallery__link" href="${original}">
 		<img
-			class="gallery__image"
-			src="${preview}"
-			data-source="${original}"
-			alt="${description}"
+		    class="gallery__image"
+		    src="${preview}"
+		    data-source="${original}"
+		    alt="${description}"
 		/>
 		</a>
 		</div>`
